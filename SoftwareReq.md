26c349b63ef0743297bdb51416438bf50c88d75c
Anthony Turinski <nodeathking@gmail.com>
2021-11-11 05:13:38 -0800

    got a version of it working through JWT and credentials supplied through a .env file

--- SoftwareReq.md
+++
@@ -1,7 +0,0 @@
-# Vision
-
-1. What is the vision of this product?
-
-What pain point does this project solve?
-
-Why should we care about your product?
--- server.js
+++ server.js
@@ -1,5 +1,73 @@
 'use strict';
 
+const { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+
+// Clients id and secret from OAuth
+// replace with user OAuth later
+const oAuth2Client = new OAuth2(
+  '774089597563-vfgg6t8ko1jdpjajqareefrhjpub04cc.apps.googleusercontent.com',
+  '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'
+);
+
+oAuth2Client.setCredentials({
+  refresh_token:
+    '1//04Z_M4ot-uiBZCgYIARAAGAQSNwF-L9IrFJM0xdGESI2AbGIjeq9bxxaTPghrpzWin-XDrWZByB59OoDdcd5-OefAQF2WWSrDz2s'
+});
+
+const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
+
+const eventStartTime = new Date();
+eventStartTime.setDate(eventStartTime.getDay() + 2);
+
+const eventEndTime = new Date();
+eventEndTime.setDate(eventEndTime.getDay() + 2);
+eventEndTime.setMinutes(eventEndTime.getMinutes() + 45);
+
+const event = {
+  summary: 'Meeting with joe',
+  location: '177 Northwood Lane, Palmyra, NE 68418',
+  description: 'meeting with dave to talk about news',
+  start: {
+    dateTime: eventStartTime,
+    timeZone: 'America/Chicago',
+  },
+  end:{
+    dateTime: eventEndTime,
+    timeZone: 'America/Chicago',
+  },
+  colorId: 1,
+};
+
+calendar.freebusy.query(
+  {
+    resource: {
+      timeMin: eventStartTime,
+      timeMax: eventEndTime,
+      timeZone: 'America/Chicago', //google js timezone
+      items: [{id: 'primary'}],
+    },
+  },
+  (err, res) => {
+    if (err) {
+      return console.error('Free Busy Query Error: ', err);
+    }
+    const eventsArr = res.data.calendars.primary.busy;
+
+    if (eventsArr.length === 0) {
+      return calendar.events.insert({calendarId: 'primary', resource: event},
+        err => {
+          if (err) {
+            return console.error('Calendar Event creation error', err);
+          }
+          return console.log('Calendar event created');
+        }
+      );
+    }
+    return console.log(`Sorry I'm busy`);
+  }
+);
+
 
 // Express server
 const express = require('express');
@@ -15,60 +83,5 @@ app.use(cors());
 app.use(express.json());
 // const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
-const { google } = require('googleapis');
-const { OAuth2 } = google.auth;
-// Clients id and secret from OAuth
-const oAuth2Client = new OAuth2('800440768090-n9kc75hsra6cfeq27kvu0fhgc90d1cbb.apps.googleusercontent.com','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');
-oAuth2Client.setCredentials({refresh_token:'1//04ZR6IoRygN8BCgYIARAAGAQSNgF-L9Ir8SdvuqvlJZKci6ITB754N6Fj60LKWF59TMBCQs7padXdbQqr8nv1TVkZ07Y-vsEVzw'});
-
 
 app.listen(PORT, () => console.log(`Listening on Port : ${PORT}`));
-
-const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
-
-const eventStartTime = new Date();
-eventStartTime.setDate(eventStartTime.getDay() + 2);
-
-const eventEndTime = new Date();
-eventEndTime.setDate(eventEndTime.getDay() + 2);
-eventEndTime.setMinutes(eventEndTime.getMinutes() + 45);
-
-const event = {
-  summary: 'Meating with dave',
-  location: '177 Northwood Lane, Palmyra, NE 68418',
-  description: 'meeting with dave to talk about news',
-  start: {
-    dateTime: eventStartTime,
-    timeZone: 'America/Chicago',
-  },
-  end:{
-    dateTime: eventEndTime,
-    timeZone: 'America/Chicago',
-  },
-  colorId: 1,
-};
-
-calendar.freebusy.query({
-  resource:{
-    timeMin: eventStartTime,
-    timeMax: eventEndTime,
-    timeZone: 'America/Chicago', //google js timezone
-    items: [{id: 'primary'}],
-  }
-}, (err, res) => {
-  if(err) return console.error('Free Busy Error', err);
-
-  const eventsArr = res.data.calendars.primary.busy;
-
-  if (eventsArr.length === 0) return calendar.events.insert(
-    {calendarId: 'primary', resource: event},
-    err => {
-      if (err) return console.error('Calendar Event creation error', err);
-
-      return console.log('Calendar evnet created');
-    }
-  );
-  return console.log(`Sorry I'm busy`);
-});
-
-
