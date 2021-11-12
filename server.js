7e4860f9fb16f7676f9f370b26ca0f3b8489b730
Jaimierl <jaimieloney@gmail.com>
2021-11-11 21:28:53 -0500

    Building out the boiler plate

--- server.js
+++ server.js
@@ -15,16 +15,25 @@ app.use(cors());
 app.use(express.json());
 // const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
+
+// mongoose.connect(process.env.MONGO_CONNECTION_STRING,
+//   { useNewUrlParser: true, useUnifiedTopology: true }
+// );
+
+let taskModel = require('./Modules/model.js');
+let taskSchema = require('./Modules/schema.js');
+const stressFreeTaskEntry = mongoose.model('tasksdatabase', taskSchema);
+
 const { google } = require('googleapis');
 const { OAuth2 } = google.auth;
 // Clients id and secret from OAuth
-const oAuth2Client = new OAuth2('800440768090-n9kc75hsra6cfeq27kvu0fhgc90d1cbb.apps.googleusercontent.com','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');
-oAuth2Client.setCredentials({refresh_token:'1//04ZR6IoRygN8BCgYIARAAGAQSNgF-L9Ir8SdvuqvlJZKci6ITB754N6Fj60LKWF59TMBCQs7padXdbQqr8nv1TVkZ07Y-vsEVzw'});
+const oAuth2Client = new OAuth2('800440768090-n9kc75hsra6cfeq27kvu0fhgc90d1cbb.apps.googleusercontent.com', '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');
+oAuth2Client.setCredentials({ refresh_token: '1//04ZR6IoRygN8BCgYIARAAGAQSNgF-L9Ir8SdvuqvlJZKci6ITB754N6Fj60LKWF59TMBCQs7padXdbQqr8nv1TVkZ07Y-vsEVzw' });
 
 
 app.listen(PORT, () => console.log(`Listening on Port : ${PORT}`));
 
-const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
+const calendar = google.calendar({ version: 'v3', auth: oAuth2Client });
 
 const eventStartTime = new Date();
 eventStartTime.setDate(eventStartTime.getDay() + 2);
@@ -41,7 +50,7 @@ const event = {
     dateTime: eventStartTime,
     timeZone: 'America/Chicago',
   },
-  end:{
+  end: {
     dateTime: eventEndTime,
     timeZone: 'America/Chicago',
   },
@@ -49,19 +58,19 @@ const event = {
 };
 
 calendar.freebusy.query({
-  resource:{
+  resource: {
     timeMin: eventStartTime,
     timeMax: eventEndTime,
     timeZone: 'America/Chicago', //google js timezone
-    items: [{id: 'primary'}],
+    items: [{ id: 'primary' }],
   }
 }, (err, res) => {
-  if(err) return console.error('Free Busy Error', err);
+  if (err) return console.error('Free Busy Error', err);
 
   const eventsArr = res.data.calendars.primary.busy;
 
   if (eventsArr.length === 0) return calendar.events.insert(
-    {calendarId: 'primary', resource: event},
+    { calendarId: 'primary', resource: event },
     err => {
       if (err) return console.error('Calendar Event creation error', err);
 
@@ -71,4 +80,12 @@ calendar.freebusy.query({
   return console.log(`Sorry I'm busy`);
 });
 
+
+// Route Setups
+// Get/Put/Post/Delete Routes
+
+// Seed Array if Any - maybe an enter tasks here? or tester?
+
+// Functions:
+// Get Tasks(inc.Auth) / post tasks / delete tasks / put tasks
 
