91c257388669d9d77ae2086b302f8a101b249f7f
Jaimierl <jaimieloney@gmail.com>
2021-11-16 21:55:37 -0500

    adding occupado to schema and seed

--- modules/schema.js
+++ modules/schema.js
@@ -3,18 +3,19 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-  summary: {type: String},
-  location: {type: String},
-  description: {type: String},
+  summary: { type: String },
+  location: { type: String },
+  description: { type: String },
+  occupado: { type: Number },
   start: {
-    dateTime: {type: String},
-    timeZone: {type: String},
+    dateTime: { type: String },
+    timeZone: { type: String },
   },
-  end:{
-    dateTime: {type: String},
-    timeZone: {type: String},
+  end: {
+    dateTime: { type: String },
+    timeZone: { type: String },
   },
-  colorId: {type: String},
+  colorId: { type: String },
 });
 
 const Event = mongoose.model('Event', eventSchema);
